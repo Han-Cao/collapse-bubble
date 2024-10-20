0a5db4294e5f6316adb8630b7c35abd0db01fd21
HanC <caohan7659@gmail.com>
2024-10-20 20:57:05 +0800

    [UPD]: update help doc

--- scripts/collapse_bubble.py
+++ scripts/collapse_bubble.py
@@ -498,11 +498,11 @@ if __name__ == '__main__':
     io_arg.add_argument('--info', metavar='TAG', help='Comma-separated INFO/TAG list to include in the output map. Default: None', type=str, default=None)
 
     collapse_arg = parser.add_argument_group('Collapse arguments')
-    collapse_arg.add_argument('-l', '--min-len', metavar='INT', help='Minimum allele length to define SV, defined as max(len(alt), len(ref)). Default: 50', type=int, default=50)
-    collapse_arg.add_argument('-r', '--refdist', metavar='INT', help='Max reference location distance. Default: 100', type=int, default=100)
-    collapse_arg.add_argument('-p', '--pctseq', metavar='INT', help='Min percent sequence similarity (REF for DEL, ALT for other SVs). Default: 0.9', type=float, default=0.9)
-    collapse_arg.add_argument('-P', '--pctsize', metavar='INT', help='Min percent size similarity (SVLEN for INS, DEL; REFLEN for INV, COMPLEX). Default: 0.9', type=float, default=0.9)
-    collapse_arg.add_argument('-O', '--pctovl', metavar='INT', help='Min pct reciprocal overlap. Default: 0.9', type=float, default=0.9)
+    collapse_arg.add_argument('-l', '--min-len', metavar='50', help='Minimum allele length of variants to be included, defined as max(len(alt), len(ref)). Default: 50', type=int, default=50)
+    collapse_arg.add_argument('-r', '--refdist', metavar='100', help='Max reference location distance. Default: 100', type=int, default=100)
+    collapse_arg.add_argument('-p', '--pctseq', metavar='0.9', help='Min percent sequence similarity (REF for DEL, ALT for other SVs). Default: 0.9', type=float, default=0.9)
+    collapse_arg.add_argument('-P', '--pctsize', metavar='0.9', help='Min percent size similarity (SVLEN for INS, DEL; REFLEN for INV, COMPLEX). Default: 0.9', type=float, default=0.9)
+    collapse_arg.add_argument('-O', '--pctovl', metavar='0.9', help='Min pct reciprocal overlap. Default: 0.9', type=float, default=0.9)
 
     args = parser.parse_args()
     main(args)
